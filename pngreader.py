840ccc4083754ac9d93323a444ffdd33d4762ff3
Verte Vero <vertevero@gmail.com>
2011-09-26 19:45:42 -0500

    Python whines about Copyright symbol

--- pngreader.py
+++ pngreader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright © 2011 vertevero
+# Copyright (C) 2011 vertevero
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
